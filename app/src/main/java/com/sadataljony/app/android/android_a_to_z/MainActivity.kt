ac95c990e8fb80647833c6700a0477e11c4a44f6
sadataljony <sadataljony@.com>
2022-12-08 21:23:07 +0600

    Initial commit

---
+++ app/src/main/java/com/sadataljony/app/android/android_a_to_z/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sadataljony.app.android.android_a_to_z
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
