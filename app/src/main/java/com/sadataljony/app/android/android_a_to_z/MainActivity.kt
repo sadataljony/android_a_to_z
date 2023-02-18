771199343f5b411f66928948f9b86fb0a9342708
sadataljony <sadataljony@.com>
2023-02-18 16:30:47 +0600

    add floating action button

--- app/src/main/java/com/sadataljony/app/android/android_a_to_z/MainActivity.kt
+++ app/src/main/java/com/sadataljony/app/android/android_a_to_z/MainActivity.kt
@@ -2,10 +2,24 @@ package com.sadataljony.app.android.android_a_to_z
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.sadataljony.app.android.android_a_to_z.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.fab.setOnClickListener {
+            Toast.makeText(
+                this@MainActivity,
+                "Floating Action Button Working",
+                Toast.LENGTH_LONG
+            ).show()
+        }
     }
 }
